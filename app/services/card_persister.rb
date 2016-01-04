9cec4ad213f8b129f3f92ec3cea52ab40d9ab9ba
Chris Pomfret <chris.pomfret@bt.com>
2016-01-04 16:32:34 +0000

    persister saves collaberations unless one exsits without being modified

--- app/services/card_persister.rb
+++ app/services/card_persister.rb
@@ -1,9 +1,24 @@
 class CardPersister
   def self.save collaberations
     collaberations.each do |collaberation_hash|
+      next if card_exists_with_collaberators_and_unmodified? collaberation_hash
+      create_collaberation_for collaberation_hash
+    end
+  end
+
+  def self.card_exists_with_collaberators_and_unmodified? collaberation_hash
+    collaberations = Collaberation.where(card: collaberation_hash[:card])
+    collaberations.each do |collaberation|
+      return true if collaberation.last_activity_date == collaberation_hash[:last_activity_date]
+    end
+    false
+  end
+
+  def self.create_collaberation_for collaberation_hash
       collaberation_members = collaberation_hash.delete(:members)
       collaberation = Collaberation.new(collaberation_hash)
       collaberation.members = collaberation_members
-    end
   end
+
+  private_class_method :create_collaberation_for
 end
--- spec/app/services/card_persister_spec.rb
+++ spec/app/services/card_persister_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 describe CardPersister do
   describe "save" do
-    let(:member_1)        { double(:member_1) }
-    let(:collaberation_1) { {card: 1, last_activity_date: Date.today, members: [member_1]} }
-    let(:collaberation)   { double(:collaberation) }
+    let(:member_1)             { double(:member_1) }
+    let(:collaberation_1)      { double(:collaberation_1, card: 1, last_activity_date: Date.today, members: [member_1]) }
+    let(:collaberation_1_hash) { {card: 1, last_activity_date: Date.today, members: [member_1]} }
+    let(:collaberation)        { double(:collaberation, last_activity_date: Date.today) }
 
     before do
       allow(Collaberation).to receive(:new).and_return(collaberation)
@@ -13,7 +14,7 @@ describe CardPersister do
     it 'saves new cards' do
       expect(Collaberation).to receive(:new).with({card: 1, last_activity_date: Date.today})
       expect(collaberation).to receive(:members=).with([member_1])
-      CardPersister.save([collaberation_1])
+      CardPersister.save([collaberation_1_hash])
     end
 
     it 'saves seperate collaberation for changing members on existing card do' do
@@ -25,7 +26,9 @@ describe CardPersister do
     end
 
     it 'does nothing when trying to save a collaberation on a card which already exists and has not been modified' do
-
+      allow(Collaberation).to receive(:where).with(card: 1).and_return [collaberation_1]
+      expect(Collaberation).not_to receive(:new)
+      CardPersister.save([collaberation_1_hash])
     end
   end
 end
