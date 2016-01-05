4daa07853beb5a0eb14e39dfe5f6622b66558b87
Chris Pomfret <chris.pomfret@bt.com>
2016-01-05 11:43:04 +0000

    build passing!

--- features/step_definitions/retrieving_cards_steps.rb
+++ features/step_definitions/retrieving_cards_steps.rb
@@ -8,9 +8,13 @@ end
 
 Then "I should see collaborations in the database" do
   expect(Collaberation.count).to eq 2
-  expect(Collaberation.first.attributes).to include("card": "565341d7b9ea6441ae681cf6", last_activity_date: Date.parse("Mon, 07 Dec 2015 11:25:47"))
-  expect(Collaberation.second.attributes).to include(card: "565d6da875cd637f3c1fcf82", last_activity_date: Date.parse("Sun, 13 Dec 2015 20:18:59"))
-  debugger
-  expect(Collaberation.fist.members).to match
-  expect(Collaberation.last.members).to match
+  expected_result = [{"card" => "565341d7b9ea6441ae681cf6", "last_activity_date" => Time.parse("Mon, 07 Dec 2015 11:25:47")},
+                     {"card" => "565d6da875cd637f3c1fcf82", "last_activity_date" => Time.parse("Sun, 13 Dec 2015 20:18:59")}]
+  expected_members = [["53cc170795d646b10dd1159e"], %w(55acd5018b4a2a1a0afe7d74 5652f11b1be7f1189b548048)]
+  Collaberation.all.each_with_index do |collaberation, index|
+    expect(collaberation.card).to eq(expected_result[index]["card"])
+    expect(collaberation.last_activity_date.to_i).to eq(expected_result[index]["last_activity_date"].to_i)
+    collaberation_members = collaberation.members.map(&:member_uuid)
+    expect(collaberation_members).to eq(expected_members[index])
+  end
 end
