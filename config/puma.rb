caa51e73cdcea4ec0d73d405b3680b9c7c9cc548
Chris Pomfret <chris.pomfret@bt.com>
2016-02-11 12:25:13 +0000

    puma logs to files

--- config/puma.rb
+++ config/puma.rb
@@ -3,3 +3,4 @@ daemonize
 pidfile File.expand_path "../tmp/pids/puma.pid", __dir__
 state_path File.expand_path "../tmp/puma.state", __dir__
 bind "unix://#{File.expand_path '../tmp/puma.sock', __dir__}"
+stdout_redirect 'log/pumout.log', 'log/pumerr.log', true
