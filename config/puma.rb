60f7007959b59775daf6d4ff6ccded400753330e
Chris Pomfret <chris.pomfret@bt.com>
2016-02-15 13:34:05 +0000

    add roles back into vagrant file + fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,6 @@
 workers 1
 daemonize
-# pidfile File.expand_path "../tmp/pids/puma.pid", __dir__
-# state_path File.expand_path "../tmp/puma.state", __dir__
-# bind "unix://#{File.expand_path '../tmp/puma.sock', __dir__}"
-# stdout_redirect 'log/pumout.log', 'log/pumerr.log', true
 pidfile File.expand_path "/home/pairstair/pairstair/current/tmp/pids/puma.pid", __dir__
 state_path File.expand_path "/home/pairstair/pairstair/current/tmp/puma.state", __dir__
-bind "unix://#{File.expand_path '/home/pairstair/pairstair/current/tmp/puma.sock', __dir__}"
+bind "unix:///home/pairstair/pairstair/current/tmp/puma.sock"
 stdout_redirect "/home/pairstair/pairstair/current/log/pumout.log", "/home/pairstair/pairstair/current/log/pumerr.log", true
